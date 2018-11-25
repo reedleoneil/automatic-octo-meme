9ee8d7420796abe7d8f44f6ff4734f4352b116e6
reedleoneil <reedleoneilpascual@yahoo.com>
2018-11-25 21:42:05 +0900

    Implement temperature sensor

--- baseNode_server/baseNode_server.ino
+++ baseNode_server/baseNode_server.ino
@@ -8,11 +8,15 @@ const uint8_t _out1 = 22;					// Led 1
 const uint8_t _out2 = 24;					// Led 2
 const uint8_t _out3 = 26;					// Led 3
 
-uint8_t _sensor1 = 0;						// Value of sensor 1			
-uint8_t _sensor2 = 0;						// Value of sensor 2
-uint8_t _sensor3 = 0;						// Value of sensor 3
+uint8_t _smoke_sensor1 = 0;					// Value of smoke sensor 1			
+uint8_t _smoke_sensor2 = 0;					// Value of smoke sensor 2
+uint8_t _smoke_sensor3 = 0;					// Value of smoke sensor 3
 
-const uint8_t _threshold = 40;					// Threshold value for sensors
+uint8_t _temp_sensor1 = 0;					// Value of temp sensor 1			
+uint8_t _temp_sensor2 = 0;					// Value of temp sensor 2
+uint8_t _temp_sensor3 = 0;					// Value of temp sensor 3
+
+const uint8_t _threshold = 40;					// Threshold value for smoke sensors
 
 RF24 radio(31, 33); 				                // nRF24L01 (CE,CSN)
 RF24Network network(radio); 					// Include the radio in the network
@@ -21,8 +25,8 @@ const uint16_t _node_address = 00; 				// Address of this node in Octal format (
 const uint8_t _channel = 90; 					// Radio frequency channel to operate
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };		// MAC Address
-IPAddress ip(192, 168, 10, 177);					// IP Address
-EthernetServer server(80);					// Initialize the Ethernet on port 80
+IPAddress ip(192, 168, 10, 177);				// IP Address
+EthernetServer server(80);					// Initialize the Ethernet on port 80 
 
 void setup() 							// Setup
 {
@@ -47,23 +51,32 @@ void rf24() {							// RF24
 	network.update();					// Pump the network regularly
 	while ( network.available() ) { 			// Is there any incoming data?
 		RF24NetworkHeader header;			// Construct a blank header
-		unsigned long data;				// Data
+		char data[8];				        // Data
 		network.read(header, &data, sizeof(data)); 	// Read the incoming data
+
+                // Parse data
+                uint8_t smoke = payload.substring(0, payload.indexOf(" ")).toInt();
+                //uint8_t temp = payload.substring(payload.indexOf(" ") + 1, payload.length()).toInt();
+                uint8_t temp = map(payload.substring(payload.indexOf(" ") + 1, payload.length()).toInt(), 0, 1024, 0, 256);
 		
-		uint8_t pin;					// Output pin	
-		uint8_t value = data >= _threshold ? HIGH : LOW;// Value of output	
+                // Set value for output
+		uint8_t pin;						
+		uint8_t value = String(data).toInt() >= _threshold ? HIGH : LOW;
 
 		switch (header.from_node) {			// Handle header and process data
 			case 1:    				
-				_sensor1 = data;		// Save value from sensor 1
+				_smoke_sensor1 = smoke;		// Save value from smoke sensor 1
+                                _temp_sensor1 = temp;		// Save value from temp sensor 1
 				pin = _out1;			// Set output
 		      		break;
 		    	case 2:    				
-				_sensor2 = data;		// Save value from sensor 2
+				_smoke_sensor2 = smoke;		// Save value from smoke sensor 2
+                                _temp_sensor2 = temp;		// Save value from temp sensor 2
 		      		pin = _out2;			// Set output
 		      		break;
 		    	case 3:    				
-		      		_sensor3 = data;		// Save value from sensor 3
+				_smoke_sensor3 = smoke;		// Save value from smoke sensor 3
+                                _temp_sensor3 = temp;		// Save value from temp sensor 3
 		      		pin = _out3;			// Set output
 				break;
 		}
@@ -79,6 +92,7 @@ void ethernet() {									// Ethernet
 		while (client.connected()) {
 			if (client.available()) {
 				char c = client.read();
+                                //Serial.print(c);
 				if (c == '\n' && currentLineIsBlank) {
 					client.println("HTTP/1.1 200 OK");		// Send a standard http response header
 					client.println("Content-Type: text/html");
@@ -86,9 +100,13 @@ void ethernet() {									// Ethernet
 					client.println("Connection: close");
 					client.println();
 	  				client.println(					// Payload in JSON format
-						"{ \"sensor1\": " + String(_sensor1) + 	
-						", \"sensor2\": " + String(_sensor2) + 
-						", \"sensor3\": " + String(_sensor3) + " }");
+						"{ \"smoke_sensor1\": " + String(_smoke_sensor1) + 	
+						", \"smoke_sensor2\": " + String(_smoke_sensor2) + 
+                                                ", \"smoke_sensor3\": " + String(_smoke_sensor3) + 
+                                                ", \"temp_sensor1\": " + String(_temp_sensor1) + 
+                                                ", \"temp_sensor2\": " + String(_temp_sensor2) + 
+                                                ", \"temp_sensor3\": " + String(_temp_sensor3) + 
+						", \"alert\": " + String(true) + " }");
 				  	break;
 				}
 				if (c == '\n') {
--- smoke_sensor/smoke_sensor.ino
+++ smoke_sensor/smoke_sensor.ino
@@ -5,15 +5,16 @@
 RF24 radio(10, 9); 							        // nRF24L01 (CE,CSN)
 RF24Network network(radio); 							// Include the radio in the network
 
-const uint16_t _node_address = 03; 						// Address of this node in Octal format ( 04,031, etc)
+const uint16_t _node_address = 01; 						// Address of this node in Octal format ( 04,031, etc)
 const uint8_t _channel = 90; 							// Radio frequency channel to operate
 
 const uint16_t _parent_address = 00;						// Address of parent node in Octal format ( 04,031, etc)
 
-const unsigned long interval = 10; //ms 					// How often to send data to the other unit
+const unsigned long interval = 1000; //ms 					// How often to send data to the other unit
 unsigned long last_sent; 							// When did we last send?
 
 void setup() {
+        Serial.begin(9600);
 	radio.begin();								// Begin operation of the nRF24L01
         radio.setDataRate(RF24_2MBPS);                                          // Set data rate
 	network.begin(_channel, _node_address); 				// Begin network (channel, node address)
@@ -24,10 +25,16 @@ void loop() {
 	unsigned long now = millis();
 	if (now - last_sent >= interval) { 					// If it's time to send a data, send it!
 		last_sent = now;
-		unsigned long sensor = analogRead(A0);				// Read value from sensor			
-		unsigned long data = map(sensor, 0, 1023, 0, 255);		// Map sensor value from 1024 to 256
+		unsigned long smoke_sensor = analogRead(A0);		        // Read value from sensor
+                unsigned long temp_sensor = analogRead(A1);			// Read value from sensor				
+		char data[8];
+                String payload = String(map(smoke_sensor, 0, 1023, 0, 255))     // Map sensor value from 1024 to 256 for smoke sensor value
+                                 + " " + 
+                                 String(temp_sensor);	                        // Temp sensor reading
+                payload.toCharArray(data, 8);                        
 		RF24NetworkHeader header1(_parent_address);			// Construct a header with node address of recipient 		
 		network.write(header1, &data, sizeof(data)); 			// Send the data
+                Serial.println(data);
 	}
 }
 
