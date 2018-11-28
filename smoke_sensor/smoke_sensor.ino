33f591f0952f16e0eb29fc3190084c77822d169e
reedleoneil <reedleoneilpascual@yahoo.com>
2018-11-28 22:56:04 +0900

    Reimplement temp sensor

--- smoke_sensor/smoke_sensor.ino
+++ smoke_sensor/smoke_sensor.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>
 #include <RF24.h>								// https://github.com/nRF24/RF24
 #include <RF24Network.h>							// https://github.com/nRF24/RF24Network
+#include <OneWire.h>                                                            // https://github.com/PaulStoffregen/OneWire
+#include <DallasTemperature.h>                                                  // https://github.com/milesburton/Arduino-Temperature-Control-Library
 
 RF24 radio(10, 9); 							        // nRF24L01 (CE,CSN)
 RF24Network network(radio); 							// Include the radio in the network
@@ -13,11 +15,16 @@ const uint16_t _parent_address = 00;						// Address of parent node in Octal for
 const unsigned long interval = 1000; //ms 					// How often to send data to the other unit
 unsigned long last_sent; 							// When did we last send?
 
+OneWire oneWire(2);
+DallasTemperature sensors(&oneWire);
+float _celcius = 0;
+
 void setup() {
         Serial.begin(9600);
 	radio.begin();								// Begin operation of the nRF24L01
         radio.setDataRate(RF24_2MBPS);                                          // Set data rate
 	network.begin(_channel, _node_address); 				// Begin network (channel, node address)
+        sensors.begin();
 }
 
 void loop() {
@@ -26,7 +33,9 @@ void loop() {
 	if (now - last_sent >= interval) { 					// If it's time to send a data, send it!
 		last_sent = now;
 		unsigned long smoke_sensor = analogRead(A0);		        // Read value from sensor
-                unsigned long temp_sensor = analogRead(A1);			// Read value from sensor				
+                unsigned long temp_sensor = analogRead(A1);			// Read value from sensor
+                sensors.requestTemperatures(); 
+                temp_sensor=sensors.getTempCByIndex(0);				
 		char data[8];							// Data
 		/* Construct payload */
                 String payload = String(map(smoke_sensor, 0, 1023, 0, 255))     // Map sensor value from 1024 to 256 for smoke sensor value
